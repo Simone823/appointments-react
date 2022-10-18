eedd53788b1358db0ab7df843221e645619b94a9
Simone823 <simone.daglio823@outlook.it>
2022-10-18 19:22:19 +0200

    import appointments app js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 
+// import appointments component
+import Appointments from './components/Appointments';
+
 function App() {
   return (
+    // App
     <div className="App">
-      
+
+      {/* Appointments */}
+      <Appointments/>
+
     </div>
   );
 }

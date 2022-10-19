444cb01dc53bc57e1041dffea4fae7ec20758e81
Simone823 <simone.daglio823@outlook.it>
2022-10-19 10:06:14 +0200

    btns appointments, refactoring data

--- src/components/Appointments.js
+++ src/components/Appointments.js
@@ -4,45 +4,33 @@ import React, {useState} from 'react';
 // appointments component
 const Appointments = () => {
 
-    // appointments
-    const appointments_data = [
-        {
-            id: 1,
-            image: 'img/tizio-palestra_naoqwo.jpg',
-            person: 'Fabio Robiola',
-            text: 'No pain no Gain XOXO PALESTRA MUSCOLI POTENZA'
-        },
-        {
-            id: 2,
-            image: 'img/tizia-cuffie_xf2bae.jpg',
-            person: 'Sabrina Cortelli',
-            text: "La musica è per l'anima quello che la ginnastica è per il corpo."
-        },
-        {
-            id: 3,
-            image: 'img/tizio-giacca_jvzktv.jpg',
-            person: 'Mike Russel',
-            text: "Non permettere a nessuno di dirti che non sei capace"
-        },
-        {
-            id: 4,
-            image: 'img/tipa-occhiali_svc9jp.jpg',
-            person: 'Rebecca Orlando',
-            text: "La fotografia è passione e amore, è l’emozione passeggera, che arriva e subito vola via."
-        },
-        {
-            id: 5,
-            image: 'img/tizio_awlq7r.jpg',
-            person: 'Christian Minerva',
-            text: "Non mangerei un hamburger nemmeno se mi pagassero 40,000 dollari"
-        },
-    ];
+    // appointments data json
+    const appointments_data = require('../appointmentsData.json');
 
     // appointments set appointments use state
     const [appointments, setAppointments] = useState(appointments_data);
 
+    // delete appointment
+    const deleteAppointment = (id) => (e) => {
+        setAppointments( (appointments) => {
+            return appointments.filter((appointment) => appointment.id !== id);
+        });
+    }
+
+    // delete all appointments
+    const deleteAllAppointments = () => (e) => {
+        setAppointments( (appointments) => {
+            return appointments.splice(appointments.length, 0);
+        } )
+    }
+
+    // reset appointments
+    const resetAppointments = () => (e) => {
+        setAppointments(appointments_data);
+    }
+
     return (
-        <section className='appointments pt-5'>
+        <div className='appointments pt-5'>
             <div className='container'>
 
                 {/* row 1 */}
@@ -58,39 +46,49 @@ const Appointments = () => {
                     <div className='col-12 col-md-8'>
                         
                         {/* card appointments */}
-                        <div className='card bg-dark shadow-lg'>
+                        <div className='card bg-dark shadow-lg mb-4'>
                             {/* list appoimnets */}
                             <ul className='list-appointments'>
                                 {/* appointments map */}
-                                {appointments.map( (appointment) => {
-                                    return (
-                                        <li className='d-flex align-items-center gap-3 px-3 py-4' key={appointment.id}>
-                                            {/* image */}
-                                            <figure className='user-image shadow flex-shrink-0'>
-                                                <img src={require(`../assets/${appointment.image}`)} alt={appointment.person}/>
-                                            </figure>
+                                {appointments.length ? (
+                                    appointments.map( (appointment) => {
+                                        return (
+                                            <li className='d-flex align-items-center gap-3 px-3 py-4' key={appointment.id}>
+                                                {/* image */}
+                                                <figure className='user-image shadow flex-shrink-0'>
+                                                    <img src={require(`../assets/${appointment.image}`)} alt={appointment.person}/>
+                                                </figure>
 
-                                            {/* description */}
-                                            <div className='description flex-grow-1'>
-                                                {/* name */}
-                                                <h5 className='mb-0 fw-bold'>{appointment.person}</h5>
-                                                <p className="mb-0">{appointment.text}</p>
-                                            </div>
+                                                {/* description */}
+                                                <div className='description flex-grow-1'>
+                                                    {/* name */}
+                                                    <h5 className='mb-0 fw-bold'>{appointment.person}</h5>
+                                                    <p className="mb-0">{appointment.text}</p>
+                                                </div>
 
-                                            {/* btn */}
-                                            <div className='buttons align-self-start'>
-                                                <button className='btn btn-danger shadow'>X</button>
-                                            </div>
-                                        </li>
-                                    )
-                                })}
+                                                {/* btn */}
+                                                <div className='button align-self-start'>
+                                                    <button onClick={deleteAppointment(appointment.id)} className='btn btn-danger shadow'>X</button>
+                                                </div>
+                                            </li>
+                                        )
+                                    })   
+                                ) : (
+                                    <h4 className='text-center fw-bold py-3'>Non ci sono appuntamenti</h4>
+                                )} 
                             </ul>
+                        </div>
+
+                        {/* btn */}
+                        <div className='buttons d-flex justify-content-between'>
+                            <button onClick={resetAppointments()} type='button' className='btn btn-info'>Reset</button>
+                            <button onClick={deleteAllAppointments()} type='button' className='btn btn-danger'>Elimina tutti</button>
                         </div>
                     </div>
                 </div>
 
             </div>
-        </section>
+        </div>
     )
 }
 
